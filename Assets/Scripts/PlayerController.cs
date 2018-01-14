edf343148ca8723ed0b7d322fc1c5fbb603fb9c0
Kohta Wajima <kohta.wajima@gmail.com>
2018-01-14 03:12:10 -0500

    Adding a Player spaceship and default speed (thru Lecture 105)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			MoveLeft();
+		}
+		else if(Input.GetKeyDown(KeyCode.RightArrow)) {
+			MoveRight();
+		}
+	}
+
+
+	void MoveLeft () {
+		transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+	}
+
+	void MoveRight () {
+		transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+	}
+}
