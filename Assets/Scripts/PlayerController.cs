97810c768643facfa4c6b8dd1266b90765f7e504
Kohta Wajima <kohta.wajima@gmail.com>
2018-01-14 04:02:03 -0500

    Spawn enemy on selected positions (thru Lecture 108)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 15.0f;
+	public float padding = 1f;
+	float xmin;
+	float xmax;
 
 	// Use this for initialization
 	void Start () {
-	
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftedge = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightedge = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xmin = leftedge.x + padding;
+		xmax = rightedge.x - padding;
 	}
 	
 	// Update is called once per frame
@@ -18,14 +25,20 @@ public class PlayerController : MonoBehaviour {
 		else if(Input.GetKeyDown(KeyCode.RightArrow)) {
 			MoveRight();
 		}
+
+		//Restrict the player to the gamespace
+		float newX = Mathf.Clamp (transform.position.x, xmin, xmax);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 
 
 	void MoveLeft () {
-		transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		//transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		transform.position += Vector3.left * speed * Time.deltaTime;
 	}
 
 	void MoveRight () {
-		transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		//transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		transform.position += Vector3.right * speed * Time.deltaTime;
 	}
 }
